20a3e7609c28746834c3c9dc058387e95dbd5905
Arafat Husayn <ArafatHusayn@users.noreply.github.com>
2016-08-14 18:42:43 +0600

    added onhover

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
     $('body').on('click', function() {
         $('div[data-testid="fbfeed_story"]').find('a[href*="/ads"]:contains("Sponsored")').closest('div[data-testid="fbfeed_story"]').css("display", "none");
     });
+    $('body').on('hover', function() {
+        $('div[data-testid="fbfeed_story"]').find('a[href*="/ads"]:contains("Sponsored")').closest('div[data-testid="fbfeed_story"]').css("display", "none");
+    });
 });
 
 $(window).scroll(function() {
